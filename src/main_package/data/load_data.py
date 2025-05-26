3d85c78d5536010efff2c1b827ceee6cf02934ed
mpenn114 <mattpenn114@gmail.com>
2025-05-26 20:52:09 +0100

    add model

--- src/main_package/data/load_data.py
+++ src/main_package/data/load_data.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from typing import List
+from datetime import datetime
+import numpy as np
 
 
 def load_data(male_data: bool) -> pd.DataFrame:
@@ -12,6 +14,7 @@ def load_data(male_data: bool) -> pd.DataFrame:
     Returns:
         pd.DataFrame: The concatenated data
     """
+    # Iterate through the saved CSVs
     file_suffix = "men" if male_data else "women"
     loaded_dfs: List[pd.DataFrame] = []
     for year in range(2019, 2025):
@@ -19,4 +22,39 @@ def load_data(male_data: bool) -> pd.DataFrame:
             pd.read_csv(f"src/main_package/data/{year}_{file_suffix}.csv")
         )
 
-    return pd.concat(loaded_dfs)
+    # Perform light processing on the data
+    combined_df = pd.concat(loaded_dfs)
+    combined_df["match_date"] = combined_df["Date"].apply(
+        lambda x: datetime.strptime(x, "%m/%d/%Y")
+    )
+    combined_df = create_estimated_true_probs(combined_df)
+
+    # Drop NA on various values
+    combined_df = combined_df.dropna(
+        subset=["Winner", "Loser", "Surface", "Tournament", "true_win_prob"]
+    )
+
+    # Return the dataframe sorted by match date
+    return combined_df.sort_values(by="match_date").reset_index(drop=True)
+
+
+def create_estimated_true_probs(combined_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Create the estimated true probabilities for results
+
+    Args:
+        combined_df (pd.DataFrame): The combined data
+
+    Returns:
+        pd.DataFrame: The original dataframe with a probability for true_win_prob
+    """
+    combined_df["true_win_prob"] = (1 / combined_df["AvgW"]).to_numpy() / (
+        (1 / combined_df[["AvgW", "AvgL"]].to_numpy()).sum(axis=1)
+    )
+
+    combined_df["additive_win_prob"] = np.log(
+        (1 - combined_df["true_win_prob"].to_numpy())
+        / combined_df["true_win_prob"].to_numpy()
+    )
+
+    return combined_df
