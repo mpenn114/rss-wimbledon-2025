86aada31af641029f0b1de96b5c572c385cf80c8
mpenn114 <78731262+mpenn114@users.noreply.github.com>
2025-08-02 13:05:04 +0100

    feat: add post-tournament analysis (#4)
    * add post tournament analysis
    
    * add longitudinal code
    
    * add draws
    
    * update readme
    
    * pre commit
    
    * pre commit
    
    * add notebook

--- src/main_package/model/predict.py
+++ src/main_package/model/predict.py
@@ -2,29 +2,55 @@ import pandas as pd
 import numpy as np
 from .utils import define_prize_money, ModelParameters
 from src.main_package.model.train import train_model
+import os
+from typing import Optional
 
 
-def predict_wimbledon_prize_money(
-    male_data: bool, model_parameters: ModelParameters
-) -> pd.DataFrame:
+def predict_tournament_prize_money(
+    male_data: bool,
+    model_parameters: ModelParameters,
+    tournament: str,
+    tournament_year: int,
+    strength_forecast_only: bool = False,
+) -> Optional[pd.DataFrame]:
     """
-    Predict the prize money that each player will get in Wimbledon
+    Predict the prize money that each player will get in the tournament
 
     Args:
         male_data (bool): Whether or not we want male data
         model_parameters (ModelParameters): The parameters to use in the odds
             forecasting model
+        tournament (str): The tournament to predict
+        tournament_year (int): The tournament year
+        strength_forecast_only (bool): Whether or not to only calculate player strengths
+            if, for example, tournament draw information is not available
+
+    Returns:
+        Optional[pd.DataFrame]: The predicted prize money from each player, if required
     """
     # Get the player strength map
     _, player_strength_map = train_model(
         temporal_decay=model_parameters["temporal_decay"],
         grass_weight=model_parameters["grass_weight"],
+        clay_weight=model_parameters["clay_weight"],
         male_data=male_data,
         return_player_strengths=True,
+        tournament=tournament,
+        tournament_year=tournament_year,
     )
 
+    # Dump the player strength maps
+    suffix = "male" if male_data else "female"
+    pd.Series(player_strength_map).to_csv(
+        f"results/player_strengths_{suffix}_{tournament}_{tournament_year}.csv"
+    )
+
+    # Stop the pipeline here if needed
+    if strength_forecast_only:
+        return
+
     # Load the draw. We expect the CSV to contain players in the same order as the tree
-    tournament_draw = load_draw(male_data)
+    tournament_draw = load_draw(male_data, tournament, tournament_year)
     player_strengths_pd = tournament_draw["player_name"].map(player_strength_map)
 
     # NB: This seems odd, but high ratings are worse throughout our model due to a
@@ -63,8 +89,19 @@ def predict_wimbledon_prize_money(
                     block_size * block_index : block_size * (block_index + 1),
                 ]
             )
-    # Calculate prize money
+    # Calculate and save the round reached
     round_reached_probability = progression[:-1] - progression[1:]
+    round_reached_dataframe = pd.DataFrame(
+        round_reached_probability.T, columns=[f"round_{x}" for x in range(8)]
+    )
+    round_reached_dataframe["player_name"] = tournament_draw["player_name"]
+    round_reached_dataframe.to_csv(
+        f"results/predicted_round_reached_dataframe_{suffix}_{tournament}_{tournament_year}.csv",
+        index=False,
+    )
+
+    # Calculate prize money
+
     prize_money = define_prize_money()
     tournament_draw["mean_prize_money"] = np.sum(
         round_reached_probability * prize_money[:, np.newaxis], axis=0
@@ -72,17 +109,30 @@ def predict_wimbledon_prize_money(
     return tournament_draw
 
 
-def load_draw(male_data: bool) -> pd.DataFrame:
+def load_draw(male_data: bool, tournament: str, tournament_year: int) -> pd.DataFrame:
     """
     Load a demo version of the draw for testing
 
     Args:
         male_data (bool): Whether or not we are using the male data
+        tournament (str): The tournament we are loading
+        tournament_year (int): The tournament year we are loading
 
     Returns:
         pd.DataFrame: The draw data
     """
     if male_data:
-        return pd.read_csv("src/main_package/data/wimbledon_male.csv")
+        file_path = (
+            f"src/main_package/data/draws/{tournament}_{tournament_year}_male.csv"
+        )
     else:
-        return pd.read_csv("src/main_package/data/wimbledon_female.csv")
+        file_path = (
+            f"src/main_package/data/draws/{tournament}_{tournament_year}_female.csv"
+        )
+
+    if not os.path.isfile(file_path):
+        raise ValueError(
+            f"Tournament {tournament} could not have prize-money forecasts performed because no draw information was found at {file_path}!"  # noqa: E501
+        )
+
+    return pd.read_csv(file_path)
